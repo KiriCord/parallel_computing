05557c5905176b3297959556495ab57654829011
KiriCord <rikudosennin410@gmail.com>
2023-04-18 23:54:48 +0500

    added an example of calculations

--- fox_algo/src/main.rs
+++ fox_algo/src/main.rs
@@ -23,13 +23,13 @@ fn _multiply(matrix_a: &Matrix<i32>, matrix_b: &Matrix<i32>) -> Matrix<i32> {
 }
 
 fn fox_algo(matrix_a: &Matrix<i32>, matrix_b: &Matrix<i32>) -> Matrix<i32> {
-    let mut matrix_c = vec![vec![0; MATRIX_SIZE]; MATRIX_SIZE];
+    let mut matrix_c = vec![vec![0; matrix_a.len()]; matrix_a.len()];
 
-    let temp_result: Vec<_> = (0..MATRIX_SIZE)
+    let temp_result: Vec<_> = (0..matrix_a.len())
         .into_par_iter()
         .map(|stage| {
             (0..matrix_a.len()).into_par_iter().map(move |i| {
-                let k = (i + stage) % MATRIX_SIZE;
+                let k = (i + stage) % matrix_a.len();
                 (0..matrix_b.len())
                     .into_par_iter()
                     .map(move |j| (i, j, matrix_a[i][k] * matrix_b[k][j]))
@@ -44,9 +44,37 @@ fn fox_algo(matrix_a: &Matrix<i32>, matrix_b: &Matrix<i32>) -> Matrix<i32> {
     }
 
     matrix_c
+
+    // for stage in 0..MATRIX_SIZE {
+    //     for i in 0..matrix_a.len() {
+    //         let mut k = (i + stage) % MATRIX_SIZE;
+    //         for j in 0..matrix_b.len() {
+    //             matrix_c[i][j] += matrix_a[i][k] * matrix_b[k][j];
+    //         }
+    //     }
+    // }
+}
+
+fn print_matrix(matrix: &Matrix<i32>) {
+    for i in 0..matrix.len() {
+        for j in 0..matrix.len() {
+            print!("{} ", matrix[i][j]);
+        }
+        println!();
+    }
+    println!();
 }
 
 fn main() {
+    {
+        let matrix_test_a = &generate_square_matrix(3, 0..10);
+        let matrix_test_b = &generate_square_matrix(3, 0..10);
+        let result = fox_algo(matrix_test_a, matrix_test_b);
+        print_matrix(matrix_test_a);
+        print_matrix(matrix_test_b);
+        print_matrix(&result);
+    }
+
     let matrix_a = &generate_square_matrix(MATRIX_SIZE, 0..100);
     let matrix_b = &generate_square_matrix(MATRIX_SIZE, 0..100);
     generate_bench!(5, [4, 8, 16], fox_algo, matrix_a, matrix_b);
